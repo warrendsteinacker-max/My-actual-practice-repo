96a2f84c93799a55f6cba83eb7177a3f6fd916d9
Warren <warrendsteinacker@gmail.com>
2025-12-30 13:42:23 -0500

    going to solve this problem

---
+++ practice.js
@@ -0,0 +1,20 @@
+function loginFlow() {
+  console.log("1. Start Login");
+
+  setTimeout(() => {
+    console.log("2. Logic Check");
+  }, 0);
+
+  Promise.resolve().then(() => {
+    console.log("3. Database Call");
+  });
+
+  console.log("4. Success");
+}
+
+loginFlow();
+// Currently logs: 1, 4, 3, 2
+
+
+///Goal: Fix the code so it logs the steps in the correct order for a user login flow:
+//  Start -> Logic -> Database -> Success.
