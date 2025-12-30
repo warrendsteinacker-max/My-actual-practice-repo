e0f7373daac9f2e24a89170ae161a3c348b9e176
Warren <warrendsteinacker@gmail.com>
2025-12-30 18:06:11 -0500

    practicing some more

--- practice.js
+++ practice.js
@@ -2,3 +2,21 @@
 //getDetails to its prototype that returns a string: "Title by Author".
 
 
+class B {
+    constructer(a, b, c, d){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+    }
+    geta() {console.log(this.a);}
+    getb() {return this.b;}
+    getc() {return this.c;}
+    getd() {return this.d;}
+}
+
+const b = new B(1, 2, 3, 4);
+
+
+
+b.geta();
