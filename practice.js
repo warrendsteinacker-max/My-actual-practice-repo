cff12383ccf24de2a3c75e40a93feb9baa018d9a
Warren <warrendsteinacker@gmail.com>
2025-12-30 14:02:05 -0500

    going to fix this

--- practice.js
+++ practice.js
@@ -1,10 +1,5 @@
-function Task(title) {
-  this.title = title;
-  // This is inefficient if we have 10,000 tasks!
-  this.save = function() {
-    console.log(`Saving ${this.title} to Book Collection...`);
-  };
+for (var i = 1; i <= 3; i++) {
+  setTimeout(() => {
+    console.log("Task Index:", i);
+  }, 1000);
 }
-
-const t1 = new Task("History");
-const t2 = "Science";
