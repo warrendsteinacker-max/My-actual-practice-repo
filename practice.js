8ecc47c783069d22b966b3eb7fc6534c3c532c4c
Warren <warrendsteinacker@gmail.com>
2025-12-30 14:16:09 -0500

    going to fix this

--- practice.js
+++ practice.js
@@ -1,13 +1,10 @@
-const vehicle = {
-  wheels: 4,
-  engine: "v6"
+const logger = {
+  reason: "User Interaction",
+  log: function() {
+    setTimeout(function() {
+      console.log("Capturing due to:", this.reason);
+    }, 1000);
+  }
 };
 
-const car1 = Object.create(vehicle);
-const car2 = Object.create(vehicle);
-
-car1.wheels = 2; // Shadowing
-delete car1.wheels;
-
-console.log(car1.wheels);
-console.log(car2.wheels);
+logger.log(); // 4
