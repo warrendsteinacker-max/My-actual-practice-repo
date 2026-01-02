e74521994b8eeac40fb453dad828c64b20ceba23
Warren <warrendsteinacker@gmail.com>
2026-01-02 09:29:15 -0500

    I am brain storming

--- practice.js
+++ practice.js
@@ -1,1 +1,3 @@
 ////for my project, I am makeing a cod zombies map tranzit
+
+////not sure how three.js works but if I were to create controls myself for player, I would use a switch stament that has the event passed down to it and useing key word key
