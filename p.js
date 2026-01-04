793b2ce84199ecac6eaf3a8c9f33cc0aecc8a4d9
Warren <warrendsteinacker@gmail.com>
2026-01-04 10:53:29 -0500

    think I made proper polyfill

--- p.js
+++ p.js
@@ -1,5 +1,12 @@
 Object.prototype.mycall = function() {
-
-    
-
+    for (let key of this){
+        console.log(this[key])
+    }
 }
+
+ob = {
+    1: "a",
+    2: "b",
+}
+
+console.log(ob.mycall)
