cbeb8c2324cd728081aac857c408091f8a90a1be
Warren <warrendsteinacker@gmail.com>
2026-01-12 06:26:41 -0500

    think I am makeing proper login controller

--- p.js
+++ p.js
@@ -1,1 +1,17 @@
-///
+const register = async(req, res) => {
+    const {email, password} = req.body
+
+    const ismatch = await User.findOne({email})
+
+    if(ismatchE){
+        return res.status(400).json({sucess: false})
+    }
+
+    const ismatchP = await bcrypt.compare(password, ismatch.password)
+
+    if(ismatchP){
+        return res.status(400).json({succes: false})   
+    }
+
+    
+}
