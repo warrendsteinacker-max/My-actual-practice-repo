68859102680aa15c187e2b9ef7bff58c02812eae
Warren <warrendsteinacker@gmail.com>
2026-01-12 06:37:39 -0500

    working on it

--- p.js
+++ p.js
@@ -2,7 +2,11 @@ const register = async(req, res) => {
     try{
         const {email, password, username} = req.body
 
-        const isuser = 
+        const isuser = await User.findOne({email})
+            if(isuser){
+                return res.status(400).json({S: false})
+            }
+        
     }
     catch(error){
 
