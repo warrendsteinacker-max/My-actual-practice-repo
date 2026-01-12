3733f6c28312ed712b255401dfc5a7b103d7e30e
Warren <warrendsteinacker@gmail.com>
2026-01-12 06:34:17 -0500

    going to redo it

--- p.js
+++ p.js
@@ -1,18 +1,10 @@
 const register = async(req, res) => {
-    const {email, password} = req.body
+    try{
+        const {email, password, username} = req.body
 
-    const ismatch = await User.findOne({email})
-
-    if(ismatchE){
-        return res.status(400).json({sucess: false})
+        const isuser = 
     }
+    catch(error){
 
-    const ismatchP = await bcrypt.compare(password, ismatch.password)
-
-    if(ismatchP){
-        return res.status(400).json({succes: false})   
     }
-
-    const user = new User({email, password})
-    const newuser = awiat create(user) 
 }
