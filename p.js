77a225d291df885dc41acd1aed64057e05361bdd
Warren <warrendsteinacker@gmail.com>
2026-01-13 13:12:28 -0500

    think this will wowrk

--- p.js
+++ p.js
@@ -72,3 +72,14 @@ const Tcheak = (req, res, next) => {
 
 
 
+const Rcheak = async(req, res, next) => {
+    try{
+        if(req.user.role === admin){
+            next()
+        }
+    }
+    catch(error){
+        console.error(error.message)
+        return res.status(400).json({permision: false})
+    }
+}
