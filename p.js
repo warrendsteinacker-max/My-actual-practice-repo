a8534893d1792f50b410a36c29cec08c42ad2196
Warren <warrendsteinacker@gmail.com>
2026-01-13 10:13:22 -0500

    think this would work

--- p.js
+++ p.js
@@ -1,14 +1,14 @@
 const Uuser = async(req, res) => {
     const {email, pass} = req.body
 
-    
-    if(){
-
+    try{
+        const salt = await bcrypt.genSalt(10)
+        const Hpass = bcrypt.hash(pass, salt)
+        const Euser = await Users.findOneByIdAndUpdate(req.user.id, {p: Hpass}, {e: email})
+        res.status(200).json({T: true})
     }
-
-    if(){
-
+    catch(error){
+        console.error(error.message)
+        return res.status(400).json({E: false})       
     }
-
-    const Euser = await Users.findOneByIdAndUpdate()  
 }
